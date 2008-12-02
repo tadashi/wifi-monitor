9587fd26cf0fd36966db8e8d73dd979a5a780fa2
tadashi <tadashi>
2008-12-02 06:34:50 +0000

    add config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+import os
+import re
+
+class Configure(object):
+    def __init__(self, interface, threshold):
+        super(FrameFilter, self).__init__()
+
+        self.int = interface
+        self.thr = threshold
+
+        self.ip_addr = ''
+        self.ether_addr =''
+
+        
+    def get_addr(self):
+        
+        p = os.popen("/sbin/ifconfig %s") % self.int
+        t = p.read()
+        p.close()
+
+        self.ip_addr =  re.search("inet addr:([0-9.]+)",t).group(1)
+        self.ether_addr = re.search("HWaddr ([0-9a-fA-F:]+)", t).group(1)
